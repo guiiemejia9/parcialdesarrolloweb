ac0268dc4a87dd260366f77bad2086cd45811215
guiiemejia9 <80605735+guiiemejia9@users.noreply.github.com>
2022-08-06 13:41:51 -0600

    Se configura Controlador y se mejora formulario

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-    <form class="w-50 mx-auto mt-5 pt-5" action="{{ route('Books-create') }}" method="POST">
+    <form class="w-50 mx-auto mt-5 pt-5" action="{{ route('books-create') }}" method="POST">
         @csrf
         <h3 class="text-center my-1">Registro de Libro</h3>
         @include('messeges.messages')
@@ -12,29 +12,40 @@
         <div class="form-group">
             <label for="name">Nombre</label>
             <input type="text" class="form-control {{$errors->has('name')? 'is-invalid':''}}" name="name" id="name"
-                   value="{{isset($customer->name)?$customer->name:old('name') }}">
+                   value="{{isset($books->name)?$books->name:old('name') }}">
         </div>
 
         <div class="form-group">
             <label for="ediction">Edicion</label>
             <input type="text" class="form-control {{$errors->has('ediction')? 'is-invalid':''}}" name="ediction" id="ediction"
-                   value="{{isset($Books->ediction)?$Books->ediction:old('ediction') }}" >
+                   value="{{isset($books->ediction)?$books->ediction:old('ediction') }}" >
         </div>
 
 
         <div class="form-group">
             <label for="year">Año</label>
             <input type="number" class="form-control {{$errors->has('year')? 'is-invalid':''}}" name="year" id="year"
-                   value="{{isset($Books->year)?$Books->year:old('Year') }}">
+                   value="{{isset($books->year)?$books->year:old('Year') }}">
         </div>
 
 
         <div class="form-group">
             <label for="year">Author</label>
             <input type="number" class="form-control {{$errors->has('author')? 'is-invalid':''}}" name="year" id="author"
-                   value="{{isset($Books->author)?$Books->author:old('author') }}">
+                   value="{{isset($books->author)?$books->author:old('author') }}">
         </div>
+        <div class="form-group hidden">
+            <label for="name">Editorial</label>
+            <select name="Editorial_id_editorial" id="Editorial_id_editorial" class="form-control form-control-lg"
+                    value="{{isset($books->Editorial_id_editorial)?$books->Editorial_id_editorial:old('Editorial_id_editorial') }}">
+                <option selected>selecione</option>
+                @foreach ($editorials as $editorial)
 
+                    <option value="{{ $editorial->Editorial_id_editorial }}">{{ $editorial->name }}</option>
+
+                @endforeach
+            </select>
+        </div>
 
 
         <button type="submit" class="btn btn-primary">Registrar</button>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\BooksController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('Libros')->group(function () {
+    Route::get('/agregar', [BooksController::class, 'create'])->name('books-add');
+    Route::get('/visualizar', [BooksController::class, 'index'])->name('books-visualize');
+    Route::post('/crear', [BooksController::class, 'store'])->name('books-create');
 });
